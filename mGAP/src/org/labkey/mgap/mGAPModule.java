06bff4cc7617020171a870b6d0e3a33f8da8cc2b
bbimber <bbimber@gmail.com>
2025-02-20 15:04:01 -0800

    Modify ContentSecurityPolicyFilter

--- mGAP/src/org/labkey/mgap/mGAPModule.java
+++ mGAP/src/org/labkey/mgap/mGAPModule.java
@@ -108,7 +108,7 @@ public class mGAPModule extends ExtendedSimpleModule
 
         SystemMaintenance.addTask(new mGapMaintenanceTask());
 
-        ContentSecurityPolicyFilter.registerAllowedSources(Directive.Connection, this.getClass().getName(), "https://*.fontawesome.com", "https://code.jquery.com");
+        ContentSecurityPolicyFilter.registerAllowedSources(Directive.Connection, this.getClass().getName(), "https://code.jquery.com");
         ContentSecurityPolicyFilter.registerAllowedSources(Directive.Style, this.getClass().getName(), "https://code.jquery.com", "https://www.gstatic.com");
         ContentSecurityPolicyFilter.registerAllowedSources(Directive.Font, this.getClass().getName(), "https://*.fontawesome.com");
 
